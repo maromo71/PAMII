df6218439b7b2b519cadc12c289d505726f2a3b8
maromo71 <professormoraes@gmail.com>
2025-02-12 16:25:50 -0300

    Aula do dia 12 de fevereiro

---
+++ aula1202/app1/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App (){
+  return(
+    <>
+      <div className='App'>
+        <h1>Meu App</h1>
+      </div>
+      <h2>Olá Etec</h2>
+    </>
+  )
+}
+export default App
