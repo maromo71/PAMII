8200a6e079db928fe5a5eb23eef7ea0fc669329a
maromo71 <professormoraes@gmail.com>
2025-03-17 13:36:12 -0300

    Aula do dia 11 de marco de 2025

--- aula1203/sample04/src/App.jsx
+++ aula1203/sample04/src/App.jsx
@@ -1,12 +1,19 @@
-import './App.css';
-import MyRoutes from './routes';
-
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import FrontEndPage from "./pages/FrontEndPage";
+import BackEndPage from "./pages/BackEndPage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <MyRoutes />
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/front-end" element={<FrontEndPage />} />
+        <Route path="/back-end" element={<BackEndPage />} />
+      </Routes>
+    </>
   );
 }
 
